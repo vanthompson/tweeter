52b8405d761335275a4fdfd96721156d0d32008f
samskinnerphd <samskinnerphd@gmail.com>
2017-05-02 13:55:25 -0400

    add seeds for User and Tweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = User.create([{email: "lazlo@example.com", name: "Lazlo Kiss", username: "fugative",
+                      bio: "fugative from the law", location: "Atlanta", password: 'password'},
+                     {email: "mildred@example.com", name: "Mildred Hunkapillar",
+                      username: "princess", bio: "princess wannabe", location: "Roswell", 
+                      password: 'password'},
+                     {email: "gordon@example.com", name: "Gordon Ramsey",
+                      username: "chefman", bio: "chef-du-jour", location: "Birmingham",
+                      password: 'password'},
+                     {email: "rita@example.com", name: "Rita Starpattern", 
+                      username: "Boss", bio: "Mistress of the Universe", location: "Macon", 
+                      password: 'password'}])
+
+tweets = Tweet.create([{user_id: 2, message: "I am but a shadow...."}, 
+                       {user_id: 3, message: "Ohhh, that's so cute!"}, 
+                       {user_id: 4, message: "Burgers anyone?"}, 
+                       {user_id: 5, message: "Submit, Humans!  Submit!"}])
+
+
+
+
